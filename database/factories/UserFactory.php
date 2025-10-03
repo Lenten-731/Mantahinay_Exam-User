adef12ff793f655b8550b862628da7b4540685ab
Lenten-731 <myn73169@gmail.com>
2025-10-03 15:50:35 +0800

    fix: add unverified state to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -13,11 +14,18 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),          // realistic name
-            'email' => $this->faker->unique()->safeEmail(), // realistic unique email
-            'password' => Hash::make('password'),    // default password for all users
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'),
             'email_verified_at' => now(),
-            'remember_token' => \Str::random(10),
+            'remember_token' => Str::random(10),
         ];
+    }
+
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
     }
 }
